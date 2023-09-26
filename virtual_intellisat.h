af0859e33fd9fdd1fe2531eed53702a0e3552105
Jacob Tkeio <35119672+jacobtkeio@users.noreply.github.com>
2023-09-26 12:33:21 -0700

    Create virtual_intellisat header (#1)
    * new determination file
    
    * added IGRF and turned Down to Up
    
    * Add virtual_intellisat.h.
    This is the list of functions Intellisat will need to define for the ADCS Software
    
    ---------
    
    Co-authored-by: Chris-Andrade <coandrade@ucdavis.edu>
    Co-authored-by: Darsh Parikh <dvparikh@ucdavis.edu>

---
+++ virtual_intellisat.h
@@ -0,0 +1,34 @@
+/**@file virtual_intellisat.h
+ *
+ * @brief ADCS Software's interface to Intellisat.
+ *
+ * This file contains the function prototype for every
+ *  actuator-commanding and sensor-reading function
+ *  the ADCS Software needs from Intellisat.
+ *
+ * Their corresponding definitions will be written in the Intellisat
+ *  repo by the CS team. While this means any ADCS code that uses
+ *  these functions cannot run outside of the Intellisat repo, 
+ *  this approach allows for complete separation of the two repos,
+ *  which vastly simplifies development for everyone. And, while
+ *  such code will not run, it will still compile!
+ *
+ * @author Jacob Tkeio (jacobtkeio@gmail.com)
+ * @date 9/25/2023
+ */
+
+#ifndef VIRTUAL_INTELLISAT_H
+#define VIRTUAL_INTELLISAT_H
+
+//Add 'em as you need 'em!
+
+
+
+
+
+#endif//VIRTUAL_INTELLISAT_H
+
+
+
+
+
