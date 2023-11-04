a09fb8fda53679b32707fb91b8432bdda8981ed1
jacobtkeio <jacobtkeio@gmail.com>
2023-11-04 15:44:03 -0700

    Update bdot_control.c to use only magnetometer readings. (It previously used angular velocity readings from the IMU)

--- control/bdot/bdot_control.h
+++ control/bdot/bdot_control.h
@@ -21,16 +21,22 @@
 /**@brief Calculate a dipole command for all three coils given our
  *  magnetic field and angular velocity data.
  * 
- * @param mf The magnetic field vector.
+ * @param mag The current magnetic field vector.
  * 		Format: [X, Y, Z] Micro Teslas
- * @param av The angular velocity vector.
- *		Format: [X, Y, Z] Radians per Second
+ * @param last_mag The previous magnetic field vector.
+ * 		Format:    ...
+ * @param delta_t Time between the previous and current mag reading.
  * @param coils_current The vec3* that will hold the coil outputs.
  * 		Format: [X, Y, Z] Amperes
  *
  * @return Void.
  */
-void bdot_control(vec3 mf, vec3 av, vec3 *coils_current);
+void bdot_control(
+    vec3    mag,
+    vec3    last_mag,
+    double  delta_t,
+    vec3   *coils_current
+);
 
 
 #endif//BDOT_CONTROL
