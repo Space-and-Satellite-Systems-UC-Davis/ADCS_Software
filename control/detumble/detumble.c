84ee1e0d3a7380a56403b2510346b9245eafb002
charlie <charliemckennakvoriak@gmail.com>
2024-10-18 19:37:00 -0700

    modified to compile detumble.c

--- control/detumble/detumble.c
+++ control/detumble/detumble.c
@@ -12,6 +12,7 @@
 
 detumble_status detumble()
 {
+<<<<<<< HEAD
 	vec3 mag,mag_prev;
 	int delta_t;
 	vec3 angvel;
@@ -64,4 +65,58 @@ detumble_status detumble()
 	}
 
 	return DETUMBLING_SUCCESS;
+=======
+    vec3 mag,mag_prev;
+    int delta_t;
+    vec3 angvel;
+    vec3 coils_curr;
+    int curr_millis=0,previous_millis=0;
+    
+    int magx,magy,magz;
+    vi_get_angvel_status angvel_status;
+    vi_get_curr_millis_status time_status;
+    
+    time_status = vi_get_curr_millis(&curr_millis);
+     
+     if(time_status == GET_CURR_MILLIS_FAILURE)
+     {
+         return DETUMBLING_FAILURE;
+     }
+    
+    angvel_status =  vi_get_angvel(&(angvel.x), &(angvel.y), &(angvel.z));
+     
+	if(angvel_status == GET_ANGVEL_FAILURE)
+     {
+         return DETUMBLING_FAILURE;
+     }
+	
+	vi_get_mag(&(mag.x), &(mag.y), &(mag.z));
+    
+    while( 0.5 < angvel.x  || 0.5 < angvel.y || angvel.z > 0.5)
+    {
+        mag_prev = mag;
+		previous_millis = curr_millis;
+		time_status = vi_get_curr_millis(&curr_millis);
+        
+        if(time_status == GET_CURR_MILLIS_FAILURE)
+        {
+            return DETUMBLING_FAILURE;
+        }
+        
+        angvel_status =  vi_get_angvel(&(angvel.x), &(angvel.y), &(angvel.z));
+        
+		if(angvel_status == GET_ANGVEL_FAILURE)
+        {
+            return DETUMBLING_FAILURE;
+        }
+        
+		vi_get_mag(&(mag.x), &(mag.y), &(mag.z));
+        
+        delta_t=curr_millis-previous_millis;
+        
+        bdot_control(mag, mag_prev, delta_t, &coils_curr);
+    }
+
+    return DETUMBLING_SUCCESS;
+>>>>>>> 1deb436 (modified to compile detumble.c)
 }
