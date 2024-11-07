5e4c4b40c8bcf1b216dd781fe9901891abf8aa18
jacobtkeio <jacobtkeio@gmail.com>
2024-11-06 17:42:47 -0800

    Fix comment unit error in PID.h
    The time arguments to the PID functions had said seconds,
    but they should have said milliseconds.

--- control/PID/PID.h
+++ control/PID/PID.h
@@ -1,6 +1,6 @@
 /**@file PID.h
  *
- * @brief Implementation of a PID(proportional–integral–derivative) controller.
+ * @brief Implementation of a PID (proportional–integral–derivative) controller.
  *
  * @author Charles Kvoriak (charles.kvoriak@gmail.com) 9/25/2024
  */
@@ -17,14 +17,14 @@ typedef struct {
 	int t_prev;
 } PID_controller;
 
-/**@brief Generate a command for the actuator and update the internal state of the PID_controller struct.
+/**@brief Generate an actuator command.
  *
  * @param target The current target for the system. May be changed over time.
  * @param state The current state of the system.
- * @param t_curr The current time in seconds.
+ * @param t_curr The current time in system-milliseconds. Not absolute.
  * @param controller Pointer to the PID_controller struct.
  *
- * @return a command to the actuator.
+ * @return A command to the actuator.
  */
 double PID_command(
 	double target,
@@ -37,13 +37,13 @@ double PID_command(
  *
  * @param target_init The initial target for the system.
  * @param state_init The initial state of the system.
- * @param t_init The initial time in seconds.
+ * @param t_init The initial time in system-milliseconds. Not absolute.
  * @param P_gain The gain for the proportional term of the controller.
  * @param I_gain The gain for the integral term of the controller.
  * @param D_gain The gain for the derivative term of the controller.
  * @param controller Pointer to the PID_controller struct.
  *
- * @return a command to the actuator.
+ * @return A command to the actuator.
  */
 void PID_init(
 	double target_init,
