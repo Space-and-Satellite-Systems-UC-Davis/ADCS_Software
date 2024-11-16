fa8408b819d6659d325779e68227bda43c10e95b
jacobtkeio <jacobtkeio@gmail.com>
2024-11-15 17:08:44 -0800

    Fix error guard in igrf_set_date_time
    Problem: The guard that returns an error if the given time
    is not appropriate to run IGRF only returns an error if
    EVERY error condition is true.
    
    Solution: Make the guard return an error if ANY of the error
    conditions are true. All '&&' -> '||'.
    
    Additionally, see https://github.com/risherlock/ADCS/issues/1

--- determination/mag_lookup/mag_lookup.c
+++ determination/mag_lookup/mag_lookup.c
@@ -242,10 +242,10 @@ uint8_t igrf_set_date_time(uint16_t year, uint8_t month, uint8_t day,
 {
     decimal_years = 0;
 
-    if ((year < IGRF_START_YEAR) && (year > IGRF_END_YEAR) &&
-        (month > 12) && (day > 31) && (hour > 23) && (min > 60) && (sec > 60))
+    if ((year < IGRF_START_YEAR) || (year > IGRF_END_YEAR) ||
+        (month > 12) || (day > 31) || (hour > 23) || (min > 60) || (sec > 60))
     {
-        return 0; //TODO: verify...
+        return 0;
     }
 
     // Count complete years
