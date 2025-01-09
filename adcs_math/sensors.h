38d6f3380bb145b9e0db2575961c4a75cd4d69a9
Tarini Maram <56371094+tarinimaram@users.noreply.github.com>
2025-01-08 16:11:53 -0800

    pass in prev and curr values directly, remove sensor struct

--- adcs_math/sensors.h
+++ adcs_math/sensors.h
@@ -8,31 +8,28 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 
-typedef struct Sensor {
-    float currValue;
-    float prevValue;
-} sensor;
-
 /**
  * @brief Implement lowpass filter on sensor raw values to mitigate the effect of noise from abnormally high values
  * 
- * @param s sensor to filter values from
+ * @param currValue current sensor raw value
+ * @param prevValue previous sensor raw value
  * @param filterConstant constant between 0-1; a lower value is a greater damp on unusually large jumps in sensor value
  * 
  * @return filtered sensor value
 */
-float lowpass_filter(sensor s, float filterConstant);
+float lowpass_filter(float currValue, float prevValue, float filterConstant);
 
 /**
  * @brief Calculates sensor calibration value after filtering data through simple lowpass filter with default constant 0.5
  * 
- * @param s sensor to get raw value from 
+ * @param currValue current sensor raw value
+ * @param prevValue previous sensor raw value
  * @param offset constant the sensor reads when it is not supposed to read anything
  * @param gain constant multiplier for sensor raw values
  * 
  * @return sensor calibration value after filtration 
 */
-float get_sensor_calibration(sensor s, float offset, float gain);
+float get_sensor_calibration(float currValue, float prevValue, float offset, float gain);
 
 #endif
 
