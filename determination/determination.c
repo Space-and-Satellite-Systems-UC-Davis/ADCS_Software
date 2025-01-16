5c2d431d7592af1e890192e46424ffa63225759a
Tarini Maram <56371094+tarinimaram@users.noreply.github.com>
2025-01-15 17:51:57 -0800

    implement cache for determination pos, sun, and mag calculations

--- determination/determination.c
+++ determination/determination.c
@@ -13,6 +13,24 @@
 #include "ADCS.h"
 
 
+typedef struct Determination_Cache {
+    double last_update_time;
+
+    // for pos_lookup 
+    double longitude;
+    double latitude;
+    double altitude;
+    double geocentric_radius;
+    double geocentric_latitude;
+    pos_lookup_status pos_status;
+
+    vec3 reference_sun;
+    sun_lookup_status sun_status;
+    vec3 reference_mag;
+} determination_cache;
+
+static determination_cache cache;
+
 determination_status
 determination(
     int year,
@@ -25,15 +43,23 @@ determination(
     vec3 measured_sun,
     mat3 *attitude
 ) {
+
     double UTC = julian_date(
         year, month, day,
         hour + minute/60.0 + second/3600.0
     );
 
+    int recent_lookup = 0; //false
+
+    if ((UTC - cache.last_update_time) <= julian_date(0, 0, 0, 5/60.0)) {
+        recent_lookup = 1; //true
+    } else {
+        cache.last_update_time = UTC;
+    }
+    
     int update_IGRF = 0; //false
     char *tle_line1;
     char *tle_line2;
-
 
     vi_get_TLE_status tle_status =
     vi_get_TLE(tle_line1, tle_line2);
@@ -45,25 +71,41 @@ determination(
         // update IGRF when we get a new TLE, including the 1st time
     }
 
-
     double longitude;
     double latitude;
     double altitude;
     double geocentric_radius;
     double geocentric_latitude;
+    pos_lookup_status pos_status;
 
-    pos_lookup_status pos_status =
-    pos_lookup(
-    	tle_line1,
-    	tle_line2,
-    	UTC,
-    	0.0,
-    	&longitude,
-    	&latitude,
-    	&altitude,
-        &geocentric_radius,
-        &geocentric_latitude
-    );
+    if (recent_lookup) {
+        longitude = cache.longitude;
+        latitude = cache.latitude;
+        altitude = cache.altitude;
+        geocentric_radius = cache.geocentric_radius;
+        geocentric_latitude = cache.geocentric_latitude;
+        pos_status = cache.pos_status;
+    } else {
+        pos_status =
+        pos_lookup(
+            tle_line1,
+            tle_line2,
+            UTC,
+            0.0,
+            &longitude,
+            &latitude,
+            &altitude,
+            &geocentric_radius,
+            &geocentric_latitude
+        );
+
+        cache.pos_status = pos_status;
+        cache.longitude = longitude;
+        cache.latitude = latitude;
+        cache.altitude = altitude;
+        cache.geocentric_radius = geocentric_radius;
+        cache.geocentric_latitude = geocentric_latitude;
+    }
 
     switch (pos_status) {
         case SGP4_ERROR:            return DET_POS_LOOKUP_ERROR;
@@ -74,20 +116,29 @@ determination(
 
 
 	vec3 reference_sun;
+    sun_lookup_status sun_status;
 
-    sun_lookup_status sun_status =
-    sun_lookup(
-    	longitude,
-    	latitude,
-    	altitude,
-    	year,
-    	month,
-    	day,
-    	hour,
-    	minute,
-    	second,
-	    &reference_sun
-    );
+    if (recent_lookup) {
+        reference_sun = cache.reference_sun;
+        sun_status = cache.sun_status;
+    } else {
+        sun_status =
+        sun_lookup(
+            longitude,
+            latitude,
+            altitude,
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            second,
+            &reference_sun
+        );
+
+        cache.sun_status = sun_status;
+        cache.reference_sun = reference_sun;
+    }
 
     switch (sun_status) {
         case SUN_LOOKUP_BAD_DATE:       return DET_POS_LOOKUP_ERROR;
@@ -120,14 +171,18 @@ determination(
         }
     } 
 
-    igrf_update(
-        geocentric_latitude,
-        longitude,
-        geocentric_radius,
-        update_IGRF, //recalculate coefficients only on new TLE
-		&reference_mag
-    );
-
+    if (recent_lookup) {
+        reference_mag = cache.reference_mag;
+    } else {
+        igrf_update(
+            geocentric_latitude,
+            longitude,
+            geocentric_radius,
+            update_IGRF, //recalculate coefficients only on new TLE
+            &reference_mag
+        );
+        cache.reference_mag = reference_mag;
+    }
 
     triad_run_status triad_status = 
     triad(
