50dd9332d08321df889b7671baf5d44f049f00aa
Aryan Mondkar <aryan.mondkar@gmail.com>
2025-01-17 14:35:47 -0800

    Initial Files created for sun_sensors.c and sun_sensors.h. No content currently but headers have been created

---
+++ adcs_math/sun_sensors.c
@@ -0,0 +1,6 @@
+/**@file sun_sensors.c
+ *
+ * @brief Implementation of Sun Sensors calibration and data aquisition logic.
+ *
+ * @author Aryan Mondkar (aryan.mondkar@gmail.com) 1/17/2025
+ */
---
+++ adcs_math/sun_sensors.h
@@ -0,0 +1,6 @@
+/**@file quaternion.h
+ *
+ * @brief Interface for Sun Sensors calibration and data aquisition logic.
+ *
+ * @author Aryan Mondkar (aryan.mondkar@gmail.com) 1/17/2025
+ */
